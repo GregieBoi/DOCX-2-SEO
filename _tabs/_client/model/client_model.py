8dc291cdf5317ffca0544c1447f1d30ed837556f
Greg <mcgreggorkennison@gmail.com>
2025-01-30 10:48:50 -0600

    ClientModel should be mostly finished

--- _tabs/_client/model/client_model.py
+++ _tabs/_client/model/client_model.py
@@ -1,12 +1,31 @@
+import sys
+import os
+import shutil
+import __main__
+import json
 
+# responsibilites of the client model
+# 1. Store the attributes of the selected client or a potential new client
+# 2. Provide a way to save the attributes to an entry in a directory
+# 3. Provide a way to load the attributes from an entry in a directory
+# 4. Provide a way to fetch all possible clients from a directory
+# 5. Delete a client from the directory
 
 class ClientModel:
   def __init__(self):
-    self.clientName = ""
-    self.currentClient = ""
-    self.clientButton = ""
-    self.clientStyle = ""
-    self.clientWrapper = ""
+    self.clientDirectory: str = self.findMWD()
+    self.clientList: list[str] = self.fetchClientList()
+    self.clientName: str = ""
+    self.currentClient: str = ""
+    self.clientButton: str = ""
+    self.clientStyle: str = ""
+    self.clientWrapper: str = ""
+
+  def setClientList(self, clientList: list[str]):
+    self.clientList = ['New Client'].append(clientList)
+
+  def getClientList(self):
+    return self.clientList
 
   def setClientName(self, name: str):
     self.clientName = name
@@ -38,9 +57,125 @@ class ClientModel:
   def getClientWrapper(self):
     return self.clientWrapper
   
+  # reset the client attributes
   def clear(self):
+    self.clientList = self.fetchClientList()
     self.clientName = ""
-    self.currentClient = ""
+    self.currentClient = self.clientList[0]
     self.clientButton = ""
     self.clientStyle = ""
     self.clientWrapper = ""
+
+  # find the main window directory
+  def findMWD(self):
+    if getattr(sys, 'frozen', False):
+      return os.path.join(os.path.dirname(sys.executable), 'CLIENTS')
+    return os.path.join(os.path.dirname(__main__.__file__), 'CLIENTS')
+
+  # save the client attributes to a new directory
+  def saveClient(self):
+    # create a new directory with the name of the client
+    newClientDir = os.path.join(self.clientDirectory, self.clientName)
+    os.mkdir(newClientDir)
+
+    with open(os.path.join(newClientDir, 'topics.json'), 'w') as f:
+      f.write('{}')
+      f.close()
+    with open(os.path.join(newClientDir, 'button.html'), 'w') as f:
+      f.write(self.clientButton)
+      f.close()
+    with open(os.path.join(newClientDir, 'styles.json'), 'w') as f:
+      styleJSON = json.loads(self.clientStyle)
+      json.dump(styleJSON, f, indent=2)
+      f.close()
+    with open(os.path.join(newClientDir, 'wrapper.html'), 'w') as f:
+      f.write(self.clientWrapper)
+      f.close()
+
+    # load the client attributes
+    self.loadClient(self.clientName)
+
+  # load the client attributes from a directory
+  def loadClient(self, clientName):
+    # clear the client attributes
+    self.clear()
+
+    # set the currentClient to the clientName
+    self.currentClient = clientName
+    self.clientName = clientName
+
+    # read the client attributes from the directory and set the attributes
+    with open(os.path.join(self.clientDirectory, clientName, 'button.html'), 'r') as f:
+      self.clientButton = f.read()
+      f.close()
+    with open(os.path.join(self.clientDirectory, clientName, 'styles.json'), 'r') as f:
+      self.clientStyle = f.read()
+      f.close()
+    with open(os.path.join(self.clientDirectory, clientName, 'wrapper.html'), 'r') as f:
+      self.clientWrapper = f.read()
+      f.close()
+
+  # update the client attributes
+  def updateClient(self):
+    # check if currentClient is same as clientName
+    if self.currentClient == self.clientName:
+      with open(os.path.join(self.clientDirectory, self.currentClient, 'button.html'), 'w') as f:
+        f.write(self.clientButton)
+        f.close()
+      with open(os.path.join(self.clientDirectory, self.currentClient, 'styles.json'), 'w') as f:
+        styleJSON = json.loads(self.clientStyle)
+        json.dump(styleJSON, f, indent=2)
+        f.close()
+      with open(os.path.join(self.clientDirectory, self.currentClient, 'wrapper.html'), 'w') as f:
+        f.write(self.clientWrapper)
+        f.close()
+      return
+    
+    # if not create a new directory and delete the old one
+    oldClientDir = os.path.join(self.clientDirectory, self.currentClient)
+    newClientDir = os.path.join(self.clientDirectory, self.clientName)
+    newClientName = self.clientName
+
+    # remove the old client directory and create a new one
+    shutil.rmtree(oldClientDir)
+    os.mkdir(newClientDir)
+
+    # save the client attributes as files to the new directory
+    with open(os.path.join(newClientDir, 'topics.json'), 'w') as f:
+      f.write('{}')
+      f.close()
+    with open(os.path.join(newClientDir, 'button.html'), 'w') as f:
+      f.write(self.clientButton)
+      f.close()
+    with open(os.path.join(newClientDir, 'styles.json'), 'w') as f:
+      styleJSON = json.loads(self.clientStyle)
+      json.dump(styleJSON, f, indent=2)
+      f.close()
+    with open(os.path.join(newClientDir, 'wrapper.html'), 'w') as f:
+      f.write(self.clientWrapper)
+      f.close()
+
+    # clear the client attributes
+    self.clear()
+
+    # load the new client attributes
+    self.loadClient(newClientName)
+
+  # delete a client from the directory
+  def deleteClient(self):
+    # delete the client directory
+    shutil.rmtree(os.path.join(self.clientDirectory, self.currentClient))
+
+    # update the clientList
+    self.clear()
+
+  # fetch the list of clients from the directory
+  def fetchClientList(self):
+    # read the client directories from the directory and append them to list
+    clients = []
+    for entry in os.scandir(self.clientDirectory):
+      if entry.is_dir():
+        clients.append(entry.name)
+    
+    # return the list of clients
+    return ['New Client'] + (sorted(clients))
